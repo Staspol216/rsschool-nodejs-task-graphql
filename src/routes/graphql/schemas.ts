7431dca133129749ddc665411012c0efcea5d533
Polishchuk <s.polishchuk@amiga.agency>
2023-07-24 01:35:24 +0500

    feat: implement all mutations without subs

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,6 +1,7 @@
 import { Type } from '@fastify/type-provider-typebox';
-import { GraphQLBoolean, GraphQLEnumType, GraphQLFloat, GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import { GraphQLBoolean, GraphQLEnumType, GraphQLFloat, GraphQLInputObjectType, GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
 import { GraphQLContext, UUIDType } from './types/uuid.js';
+import { randomUUID } from 'crypto';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -108,7 +109,7 @@ const MemberTypeType = new GraphQLObjectType({
   })
 })
 
-export const RootQueryType = new GraphQLObjectType({
+const RootQueryType = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
       memberTypes: {
@@ -203,6 +204,225 @@ export const RootQueryType = new GraphQLObjectType({
     }
 })
 
+
+const CreatePostInputType = new GraphQLInputObjectType({
+  name: 'CreatePostInput',
+  fields: () => ({
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+    authorId: { type: UUIDType },
+  })
+})
+
+const ChangePostInputType = new GraphQLInputObjectType({
+  name: 'ChangePostInput',
+  fields: () => ({
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+  })
+})
+
+interface PostInput {
+  title: string,
+  content: string,
+  authorId: string,
+}
+
+const CreateUserInputType = new GraphQLInputObjectType({
+  name: 'CreateUserInput',
+  fields: () => ({
+    name: { type: GraphQLString },
+    balance: { type: GraphQLFloat },
+  })
+})
+
+const ChangeUserInputType = new GraphQLInputObjectType({
+  name: 'ChangeUserInput',
+  fields: () => ({
+    name: { type: GraphQLString },
+    balance: { type: GraphQLFloat },
+  })
+});
+
+interface UserInput {
+  name: string;
+  balance: number;
+}
+
+const CreateProfileInputType = new GraphQLInputObjectType({
+  name: 'CreateProfileInput',
+  fields: () => ({
+    isMale: { type: GraphQLBoolean },
+    yearOfBirth: { type: GraphQLInt },
+    memberTypeId: { type: GraphQLString },
+    userId: { type: UUIDType }
+  })
+});
+
+const ChangeProfileInputType = new GraphQLInputObjectType({
+  name: 'ChangeProfileInput',
+  fields: () => ({
+    isMale: { type: GraphQLBoolean },
+    yearOfBirth: { type: GraphQLInt },
+    memberTypeId: { type: GraphQLString }
+  })
+});
+
+interface ProfileInput {
+  isMale: boolean;
+  yearOfBirth: number;
+  memberTypeId: string;
+  userId: string;
+}
+
+const RootMutationType = new GraphQLObjectType({
+  name: 'RootMutationType',
+  fields: {
+    createPost: {
+      type: PostType,
+      args: {
+        dto: { type: new GraphQLNonNull(CreatePostInputType) }
+      },
+      async resolve(root, args: { dto: PostInput }, context: GraphQLContext) {
+        return await context.db.post.create({
+          data: {
+            id: randomUUID(),
+          ...args.dto
+          }
+        });
+      }
+    },
+    createUser: {
+      type: UserType,
+      args: {
+        dto: { type: new GraphQLNonNull(CreateUserInputType) }
+      },
+      async resolve(root, args: { dto: UserInput }, context: GraphQLContext) {
+        return await context.db.user.create({
+          data: {
+            id: randomUUID(),
+          ...args.dto
+          }
+        });
+      }
+    },
+    createProfile: {
+      type: UserType,
+      args: {
+        dto: { type: new GraphQLNonNull(CreateProfileInputType) }
+      },
+      async resolve(root, args: { dto: ProfileInput }, context: GraphQLContext) {
+        return await context.db.profile.create({
+          data: {
+            id: randomUUID(),
+          ...args.dto
+          }
+        });
+      }
+    },
+    deletePost: {
+      type: GraphQLString,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) }
+      },
+      async resolve(root, args: { id: string }, context: GraphQLContext) {
+        try {
+          await context.db.post.delete({
+            where: {
+              id: args.id,
+            },
+          });
+          return "deleted"
+        } catch {
+          return new Error("something went wrong")
+        }
+      }
+    },
+    deleteUser: {
+      type: GraphQLString,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) }
+      },
+      async resolve(root, args: { id: string }, context: GraphQLContext) {
+        try {
+          await context.db.user.delete({
+            where: {
+              id: args.id,
+            },
+          });
+          return "deleted"
+        } catch {
+          return new Error("something went wrong")
+        }
+      }
+    },
+    deleteProfile: {
+      type: GraphQLString,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) }
+      },
+      async resolve(root, args: { id: string }, context: GraphQLContext) {
+        try {
+          await context.db.profile.delete({
+            where: {
+              id: args.id,
+            },
+          });
+          return "deleted"
+        } catch {
+          return new Error("something went wrong")
+        }
+      }
+    },
+    changePost: {
+      type: PostType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+        dto: { type: new GraphQLNonNull(ChangePostInputType) }
+      },
+      async resolve(root, args: { id: string, dto: Partial<PostInput> }, context: GraphQLContext) {
+        return await context.db.post.update({
+          where: {
+            id: args.id,
+          },
+          data: args.dto
+        });
+      }
+    },
+    changeProfile: {
+      type: ProfileType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+        dto: { type: new GraphQLNonNull(ChangeProfileInputType) }
+      },
+      async resolve(root, args: { id: string, dto: Partial<ProfileInput> }, context: GraphQLContext) {
+        return await context.db.profile.update({
+          where: {
+            id: args.id,
+          },
+          data: args.dto
+        });
+      }
+    },
+    changeUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+        dto: { type: new GraphQLNonNull(ChangeUserInputType) }
+      },
+      async resolve(root, args: { id: string, dto: Partial<UserInput> }, context: GraphQLContext) {
+        return await context.db.user.update({
+          where: {
+            id: args.id,
+          },
+          data: args.dto
+        });
+      }
+    }
+  }
+})
+
 export const schema = new GraphQLSchema({
-  query: RootQueryType
+  query: RootQueryType,
+  mutation: RootMutationType,
 })
